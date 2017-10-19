529128aa7e85746e9c4d519c3f7ae75d5eb5065d
William Stam <william.stam@itna.co.za>
2017-10-19 14:42:12 +0200

    Latest

--- actors/git.php
+++ actors/git.php
@@ -38,11 +38,8 @@ class git extends \update\_ implements actorsInterface {
 			$this->output(LOG,"CHECKING GIT",_exec('git --version'));
 
 
-			if (file_exists(($this->cfg_folder)."\\.git")){
-				$this->output(LOG,"STASH",_exec('git reset --hard HEAD',$this->cfg_folder));
-			} else {
 				$this->output(LOG,"INIT",_exec('git init',$this->cfg_folder));
-			}
+				$this->output(LOG,"STASH",_exec('git reset --hard HEAD',$this->cfg_folder));
 
 			if ($this->cfg['git']['username']&&$this->cfg['git']['password']){
 				$this->output(LOG,"UPDATING",_exec('git pull https://'.$this->cfg['git']['username'] .':'.$this->cfg['git']['password'] .'@'.$this->cfg['git']['path'] .' ' . $this->cfg['git']['branch']."",$this->cfg_folder));
@@ -67,11 +64,8 @@ class git extends \update\_ implements actorsInterface {
 
 		$this->output(H4,"Self Update");
 
-		if (file_exists("\\.git")){
-			$this->output(LOG,"STASH",_exec('git reset --hard HEAD'));
-		} else {
 			$this->output(LOG,"INIT",_exec('git init'));
-		}
+			$this->output(LOG,"STASH",_exec('git reset --hard HEAD'));
 
 
 		$this->output(LOG,"SELF UPDATING",_exec('git pull https://'.$self['git']['path'] .' ' . $self['git']['branch'].""));
